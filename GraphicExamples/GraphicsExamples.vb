70841d0873475cc234a756d0175cbf7c2a6030b5
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-03-31 09:04:56 -0600

    Added other shapes

--- GraphicExamples/GraphicsExamples.vb
+++ GraphicExamples/GraphicsExamples.vb
@@ -1,3 +1,40 @@
-﻿Public Class GraphicsExamples
+﻿
+Public Class GraphicsExamples
+    Sub DrawLine()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+
+        g.DrawLine(pen, 150, 150, 100, 100)
+
+        g.Dispose()
+
+    End Sub
+    Sub DrawRectangle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black, 3)
+
+        g.DrawRectangle(pen, 150, 150, 100, 100)
+
+        g.Dispose()
+
+    End Sub
+    Sub DrawEllipse()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black, 3)
+
+        g.DrawEllipse(pen, 250, 250, 150, 150)
+
+        g.Dispose()
+
+    End Sub
+
+    Private Sub GraphicsExamples_Click(sender As Object, e As EventArgs) Handles Me.Click
+        DrawLine()
+        DrawRectangle()
+        DrawEllipse()
+    End Sub
+    Private Sub GraphicsExamples_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Me.Text = $"{e.X},{e.Y})"
+    End Sub
 
 End Class
