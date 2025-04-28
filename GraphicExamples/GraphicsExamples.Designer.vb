2f16b8eaa1696f6a24e09618ad4bf846309eb2bd
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-04-27 22:12:13 -0600

    started new batch of code

--- GraphicExamples/GraphicsExamples.Designer.vb
+++ GraphicExamples/GraphicsExamples.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class GraphicsExamples
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,202 @@ Partial Class GraphicsExamples
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
+        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ForegroundColorTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundColorTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WaveButton = New System.Windows.Forms.Button()
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ForegroundColorContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundColorContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WidthContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PickToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CustomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TopMenuStrip.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'DrawingPictureBox
+        '
+        Me.DrawingPictureBox.Location = New System.Drawing.Point(46, 67)
+        Me.DrawingPictureBox.Name = "DrawingPictureBox"
+        Me.DrawingPictureBox.Size = New System.Drawing.Size(732, 284)
+        Me.DrawingPictureBox.TabIndex = 0
+        Me.DrawingPictureBox.TabStop = False
+        '
+        'TopMenuStrip
+        '
+        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.TopMenuStrip.Name = "TopMenuStrip"
+        Me.TopMenuStrip.Size = New System.Drawing.Size(800, 24)
+        Me.TopMenuStrip.TabIndex = 1
+        Me.TopMenuStrip.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ForegroundColorTopMenuItem, Me.BackgroundColorTopMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'ForegroundColorTopMenuItem
+        '
+        Me.ForegroundColorTopMenuItem.Name = "ForegroundColorTopMenuItem"
+        Me.ForegroundColorTopMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.ForegroundColorTopMenuItem.Text = "Foreground Color"
+        '
+        'BackgroundColorTopMenuItem
+        '
+        Me.BackgroundColorTopMenuItem.Name = "BackgroundColorTopMenuItem"
+        Me.BackgroundColorTopMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.BackgroundColorTopMenuItem.Text = "Background Color"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'WaveButton
+        '
+        Me.WaveButton.Location = New System.Drawing.Point(648, 357)
+        Me.WaveButton.Name = "WaveButton"
+        Me.WaveButton.Size = New System.Drawing.Size(130, 67)
+        Me.WaveButton.TabIndex = 2
+        Me.WaveButton.Text = "Wave"
+        Me.WaveButton.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ForegroundColorContextMenuItem, Me.BackgroundColorContextMenuItem, Me.FontContextMenuItem, Me.ClearContextMenuItem, Me.WidthContextMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(171, 114)
+        '
+        'ForegroundColorContextMenuItem
+        '
+        Me.ForegroundColorContextMenuItem.Name = "ForegroundColorContextMenuItem"
+        Me.ForegroundColorContextMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.ForegroundColorContextMenuItem.Text = "Foreground Color"
+        '
+        'BackgroundColorContextMenuItem
+        '
+        Me.BackgroundColorContextMenuItem.Name = "BackgroundColorContextMenuItem"
+        Me.BackgroundColorContextMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.BackgroundColorContextMenuItem.Text = "Background Color"
+        '
+        'FontContextMenuItem
+        '
+        Me.FontContextMenuItem.Name = "FontContextMenuItem"
+        Me.FontContextMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.FontContextMenuItem.Text = "Font"
+        '
+        'ClearContextMenuItem
+        '
+        Me.ClearContextMenuItem.Name = "ClearContextMenuItem"
+        Me.ClearContextMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.ClearContextMenuItem.Text = "Clear"
+        '
+        'WidthContextMenuItem
+        '
+        Me.WidthContextMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PickToolStripMenuItem, Me.CustomToolStripMenuItem})
+        Me.WidthContextMenuItem.Name = "WidthContextMenuItem"
+        Me.WidthContextMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.WidthContextMenuItem.Text = "Width"
+        '
+        'PickToolStripMenuItem
+        '
+        Me.PickToolStripMenuItem.Name = "PickToolStripMenuItem"
+        Me.PickToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.PickToolStripMenuItem.Text = "Pick"
+        '
+        'CustomToolStripMenuItem
+        '
+        Me.CustomToolStripMenuItem.Name = "CustomToolStripMenuItem"
+        Me.CustomToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.CustomToolStripMenuItem.Text = "Custom"
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(408, 357)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(136, 67)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
         '
         'GraphicsExamples
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.WaveButton)
+        Me.Controls.Add(Me.TopMenuStrip)
+        Me.Controls.Add(Me.DrawingPictureBox)
+        Me.MainMenuStrip = Me.TopMenuStrip
         Me.Name = "GraphicsExamples"
         Me.Text = "Form1"
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TopMenuStrip.ResumeLayout(False)
+        Me.TopMenuStrip.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents DrawingPictureBox As PictureBox
+    Friend WithEvents TopMenuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ForegroundColorTopMenuItem As ToolStripMenuItem
+    Friend WithEvents BackgroundColorTopMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WaveButton As Button
+    Friend WithEvents ColorDialog As ColorDialog
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ForegroundColorContextMenuItem As ToolStripMenuItem
+    Friend WithEvents BackgroundColorContextMenuItem As ToolStripMenuItem
+    Friend WithEvents FontContextMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearContextMenuItem As ToolStripMenuItem
+    Friend WithEvents WidthContextMenuItem As ToolStripMenuItem
+    Friend WithEvents PickToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CustomToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents ClearButton As Button
 End Class
--- GraphicExamples/GraphicsExamples.vb
+++ GraphicExamples/GraphicsExamples.vb
@@ -1,40 +1,114 @@
 ﻿
 Public Class GraphicsExamples
-    Sub DrawLine()
-        Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.Black)
 
-        g.DrawLine(pen, 150, 150, 100, 100)
+    Function ForegroundColor(Optional newColor As Color = Nothing) As Color
+        Static _foreColor As Color = Color.Black
+
+        If newColor <> Nothing Then
+            _foreColor = newColor
+        End If
+
+        Return _foreColor
+    End Function
+
+    Function PenWidth(Optional newWidth As Integer = -1) As Integer
+        Static _penWidth As Integer = 2
+        If newWidth > 0 AndAlso newWidth <= 100 Then _penWidth = newWidth
+        Return _penWidth
+    End Function
+
+    Sub DrawWithMouse(oldX As Integer, oldY As Integer, newX As Integer, newY As Integer)
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics
+        Dim pen As New Pen(ForegroundColor, PenWidth())
+
+        g.DrawLine(pen, oldX, oldY, newX, newY)
 
         g.Dispose()
-
     End Sub
-    Sub DrawRectangle()
-        Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.Black, 3)
+    Sub DrawGraticule()
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
+        Dim pen As New Pen(Color.LightGray)
 
-        g.DrawRectangle(pen, 150, 150, 100, 100)
+        Dim spacingX As Double = DrawingPictureBox.Width / 10.0
+        Dim spacingY As Double = DrawingPictureBox.Height / 10.0
 
+        For i As Integer = 1 To 9
+            g.DrawLine(pen, CInt(i * spacingX), 0, CInt(i * spacingX), DrawingPictureBox.Height)
+            g.DrawLine(pen, 0, CInt(i * spacingY), DrawingPictureBox.Width, CInt(i * spacingY))
+        Next
         g.Dispose()
-
     End Sub
-    Sub DrawEllipse()
-        Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.Black, 3)
+    Sub SelectColor()
+        Dim cd As New ColorDialog()
+        If cd.ShowDialog() = DialogResult.OK Then
+            ForegroundColor(cd.Color)
+        End If
+    End Sub
+    Private Sub DisplayPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseMove, DrawingPictureBox.MouseDown
+        Static oldX, oldY As Integer
 
-        g.DrawEllipse(pen, 250, 250, 150, 150)
+        Select Case e.Button
+            Case MouseButtons.Left
+                DrawWithMouse(oldX, oldY, e.X, e.Y)
+            Case MouseButtons.Middle
+                SelectColor()
+        End Select
 
-        g.Dispose()
-
+        oldX = e.X
+        oldY = e.Y
     End Sub
 
-    Private Sub GraphicsExamples_Click(sender As Object, e As EventArgs) Handles Me.Click
-        DrawLine()
-        DrawRectangle()
-        DrawEllipse()
-    End Sub
-    Private Sub GraphicsExamples_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
-        Me.Text = $"{e.X},{e.Y})"
-    End Sub
+    '------------------------------------------------------------------------------------------------------------------------------
+    'NEWCODE ABOVE
+    '------------------------------------------------------------------------------------------------------------------------------
+    'Private Sub DrawGraticule(g As Graphics)
+    '    Dim width = DrawingPictureBox.Width
+    '    Dim height = DrawingPictureBox.Height
+
+    '    Dim hSpacing = width / 10   ' Divide into 10 vertical sections
+    '    Dim vSpacing = height / 10  ' Divide into 10 horizontal sections
+
+    '    Dim gridPen As New Pen(Color.LightGray, 1) ' Light grid color
+
+    '    ' Vertical lines
+    '    For i = 0 To 10
+    '        g.DrawLine(gridPen, CInt(i * hSpacing), 0, CInt(i * hSpacing), height)
+    '    Next
+
+    '    ' Horizontal lines
+    '    For i = 0 To 10
+    '        g.DrawLine(gridPen, 0, CInt(i * vSpacing), width, CInt(i * vSpacing))
+    '    Next
+    'End Sub
+    '' Event Handlers ----------------------------------------------------------
+
+    'Private Sub ChangeforegroundColor(sender As Object, e As EventArgs)
+    '    Dim result As DialogResult = ColorDialog.ShowDialog()
+    '    If result.ToString = "OK" Then
+    '        ForegroundColor(ColorDialog.Color)
+    '    End If
+    'End Sub
+
+    'Private Sub BackgroundColorContextMenuItem_Click(sender As Object, e As EventArgs) Handles BackgroundColorTopMenuItem.Click
+    '    Dim result As DialogResult = ColorDialog.ShowDialog()
+    '    If result.ToString = "OK" Then
+    '        BackgroundColor(ColorDialog.Color)
+    '        'This erases everything
+    '        'DrawingPictureBox.BackColor = BackgroundColor()
+    '    End If
+    'End Sub
+    'Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+    '    Me.Hide()
+    '    AboutForm.Show()
+    'End Sub
+
+
+    'Private Sub WaveButton_Click(sender As Object, e As EventArgs) Handles WaveButton.Click
+    '    ' 1. Clear the PictureBox
+    '    DrawingPictureBox.Refresh()
+
+    '    ' 2. Force a repaint (triggers DrawingPictureBox_Paint)
+    '    DrawingPictureBox.Invalidate()
+    'End Sub
 
 End Class
